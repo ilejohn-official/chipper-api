794062b005620ce971d37ebcf16de3a45cda5f86
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2026-01-05 18:26:23 +0100

    Respect deprecated and cast column

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -22,9 +22,9 @@ class FavoriteController extends Controller
     {
         $favorites = $request->user()->favorites()->get();
 
-        $postIds = $favorites->where('favoritable_type', FavoritableType::POST->value)
+        $postIds = $favorites->where('favoritable_type', FavoritableType::POST)
             ->pluck('favoritable_id');
-        $userIds = $favorites->where('favoritable_type', FavoritableType::USER->value)
+        $userIds = $favorites->where('favoritable_type', FavoritableType::USER)
             ->pluck('favoritable_id');
 
         $posts = Post::with('user')->whereIn('id', $postIds)->get();
@@ -36,9 +36,8 @@ class FavoriteController extends Controller
     public function store(CreateFavoriteRequest $request, Post $post)
     {
         $request->user()->favorites()->create([
-            'favoritable_type' => FavoritableType::POST->value,
+            'favoritable_type' => FavoritableType::POST,
             'favoritable_id' => $post->id,
-            'post_id' => $post->id
         ]);
 
         return response()->noContent(Response::HTTP_CREATED);
@@ -47,8 +46,7 @@ class FavoriteController extends Controller
     public function destroy(Request $request, Post $post)
     {
         $favorite = $request->user()->favorites()
-            ->where('post_id', $post->id)
-            ->where('favoritable_type', FavoritableType::POST->value)
+            ->where('favoritable_type', FavoritableType::POST)
             ->where('favoritable_id', $post->id)
             ->firstOrFail();
 
@@ -62,7 +60,6 @@ class FavoriteController extends Controller
         $request->user()->favorites()->create([
             'favoritable_type' => FavoritableType::USER,
             'favoritable_id' => $user->id,
-            'post_id' => 0
         ]);
 
         return response()->noContent(Response::HTTP_CREATED);
