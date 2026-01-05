a2e15d10faa104fb5c1e0f0b6f6364db5db1bad9
Opeyemi Ilesanmi <ojilesanmi7@gmail.com>
2026-01-05 12:34:40 +0100

    Leave depricated post_id column

--- database/migrations/2026_01_05_111441_add_favoritable_columns_to_favorites_table.php
+++ database/migrations/2026_01_05_111441_add_favoritable_columns_to_favorites_table.php
@@ -13,7 +13,6 @@ return new class () extends Migration {
         Schema::table('favorites', function (Blueprint $table) {
             $table->string('favoritable_type')->nullable();
             $table->unsignedBigInteger('favoritable_id')->nullable();
-            $table->unsignedBigInteger('post_id')->nullable()->change();
 
             $table->index(['favoritable_type', 'favoritable_id']);
         });
@@ -27,7 +26,6 @@ return new class () extends Migration {
         Schema::table('favorites', function (Blueprint $table) {
             $table->dropIndex(['favoritable_type', 'favoritable_id']);
             $table->dropColumn(['favoritable_type', 'favoritable_id']);
-            $table->unsignedBigInteger('post_id')->nullable(false)->change();
         });
     }
 };
